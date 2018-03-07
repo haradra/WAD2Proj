dbb21f7cbc210de844f08c4969497ce70caf32bd
Adam5i <czyzewski.i.a@gmail.com>
2018-03-07 00:07:15 +0000

    Started working on models.py; relations need to be created and structure re-discussed

--- pawpal_project/pawpal/models.py
+++ pawpal_project/pawpal/models.py
@@ -2,5 +2,58 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=200)
+    species = models.CharField(max_length=30)
+
+    def save(self, *args, **kwargs):
+        super(Pet, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Rating(models.Model):
+    petId = models.CharField(max_length=30)
+    seekerUsername  = models.CharField(max_length=30)
+    date  = models.DateField()
+    messages  = models.CharField(max_length=200)
+
+    #def save(self, *args, **kwargs):
+        #super(Rating, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.petId
+
+
+class UserProfile(models.Model):
+    location = models.CharField(max_length=128)
+    dateOfBirth = models.DateField()
+    profilePicture = models.ImageField(upload_to='profile_images', blank=True)
+    experience = models.IntegerField()
+    Description = models.CharField(max_length=200)
+    showPets = models.BooleanField()
+
+    user = models.OneToOneField(User) #is this line ok?
+    website = models.URLField(blank=True)  #should we include this line too?
+
+    def __str__(self):
+        return self.user.username
+
+
+
+class Messages(models.Model):
+    petId = models.CharField(max_length=30)
+    seekerUsername = models.CharField(max_length=30)
+    date = models.DateField()
+    url = models.URLField()
+
+    def __str__(self):
+        return self.user.username
--- pawpal_project/pawpal_project/urls.py
+++ pawpal_project/pawpal_project/urls.py
@@ -18,7 +18,6 @@ from django.conf.urls.static import static
 from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
-
 from pawpal import views
 
 urlpatterns = [
