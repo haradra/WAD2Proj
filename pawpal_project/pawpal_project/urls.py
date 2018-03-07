f1c75f70f5e219a56b0dd127b0e4dc7e85607651
Adam5i <czyzewski.i.a@gmail.com>
2018-03-07 16:52:07 +0000

    Addition of Static Dir

--- pawpal_project/pawpal_project/urls.py
+++ pawpal_project/pawpal_project/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^pawpal/', include('pawpal.urls')),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
