c0a13b5395ddb430daf1323abd2552d2b91fd5e7
Adam5i <czyzewski.i.a@gmail.com>
2018-03-07 12:18:06 +0000

    Currently working on population script

---
+++ pawpal_project/populate_pawpal.py
@@ -0,0 +1,128 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'pawpal_project.settings')
+
+import pawpal
+from django.db import models
+pawpal.setup()
+from pawpal.models import Pet, Rating, Messages, UserProfile
+
+def populate():
+    # First, we will create lists of dictionaries containing the pages
+    # we want to add into each category.
+    # Then we will create a dictionary of dictionaries for our categories.
+    # This might seem a little bit confusing, but it allows us to iterate
+    # through each data structure, and add the data to our models.
+
+    users = [
+        {"username":"Love Iguanas",
+         "password": "IreallyLoveIguanas123456789",
+         "first_name":"Donald",
+         "last_name": "Duck",
+         "last_login": models.DatetimeField(auto_now_add=True),
+         "location": "Glasgow",
+         "dateOfBirth": models.DatetimeField(auto_now_add=True),
+         "profilePicture": models.ImageField(verbose_name="picture"),
+         "experience": 4,
+         "Description": "I love Iguanas, like really. Iguanas <3",
+         "showPets": False,
+         }
+        {"username": "Doggggos",
+         "password": "IreallyLoveDoggos123456789",
+         "first_name": "Mark",
+         "last_name": "Nicold",
+         "last_login": models.DatetimeField(auto_now_add=True),
+         "location": "Glasgow",
+         "dateOfBirth": models.DatetimeField(auto_now_add=True),
+         "profilePicture": models.ImageField(verbose_name="picture"),
+         "experience": 8,
+         "Description": "Doggos, doggos, I have one, let's spend with him all our time!",
+         "showPets": True,
+         }
+        {"username": "anilano",
+         "password": "Irsdawnilano123456789",
+         "first_name": "Anilano",
+         "last_name": "Korton",
+         "last_login": models.DatetimeField(auto_now_add=True),
+         "location": "Edinburgh",
+         "dateOfBirth": models.DatetimeField(auto_now_add=True),
+         "profilePicture": models.ImageField(verbose_name="picture"),
+         "experience": 0,
+         "Description": "I'm looking for a Cat or a Dog I could take for a walk.",
+         "showPets": False,
+         }  ]
+
+
+"""
+
+    python_pages = [
+        {"title": "Official Python Tutorial",
+         "url":"http://docs.python.org/2/tutorial/",
+         "views": 1},
+        {"title":"How to Think like a Computer Scientist",
+         "url":"http://www.greenteapress.com/thinkpython/",
+         "views": 4},
+        {"title":"Learn Python in 10 Minutes",
+         "url":"http://www.korokithakis.net/tutorials/python/",
+         "views": 9} ]
+
+    django_pages = [
+        {"title":"Official Django Tutorial",
+         "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/",
+         "views": 123},
+        {"title":"Django Rocks",
+         "url":"http://www.djangorocks.com/",
+         "views": 14},
+        {"title":"How to Tango with Django",
+         "url":"http://www.tangowithdjango.com/",
+         "views": 11} ]
+
+    other_pages = [
+        {"title":"Bottle",
+         "url":"http://bottlepy.org/docs/dev/",
+         "views": 1},
+        {"title":"Flask",
+         "url":"http://flask.pocoo.org",
+         "views": 6} ]
+
+    cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
+            "Django": {"pages": django_pages, "views": 64, "likes": 32},
+            "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16} }
+
+    # If you want to add more catergories or pages,
+    # add them to the dictionaries above.
+
+    # The code below goes through the cats dictionary, then adds each category,
+    # and then adds all the associated pages for that category.
+    # if you are using Python 2.x then use cats.iteritems() see
+    # http://docs.quantifiedcode.com/python-anti-patterns/readability/
+    # for more information about how to iterate over a dictionary properly.
+
+    for cat, cat_data in cats.items():
+        c= add_cat(cat, cat_data["views"], cat_data["likes"])
+        for p in cat_data["pages"]:
+            add_page(c, p["title"], p["url"], p["views"])
+
+    # Print out the categories we have added.
+    for c in Category.objects.all():
+        for p in Page.objects.filter(category=c):
+            print("- {0} - {1}".format(str(c), str(p)))
+
+def add_page(cat, title, url, views=0):
+    p = Page.objects.get_or_create(category=cat, title=title)[0]
+    p.url=url
+    p.views=views
+    p.save()
+    return p
+
+def add_cat(name, views=0, likes=0):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.likes=likes
+    c.views=views
+    c.save()
+    return c
+"""
+# Start execution here!
+if __name__ == '__main__':
+    print("Starting Pawpal population script...")
+    populate()
