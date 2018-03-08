223999a2e4b81ad0a801824be96fc1306932cd1c
Adam5i <czyzewski.i.a@gmail.com>
2018-03-08 13:52:56 +0000

    Admin interface updated

--- pawpal_project/pawpal/admin.py
+++ pawpal_project/pawpal/admin.py
@@ -1,28 +1,27 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
-
-
 from pawpal.models import UserProfile, Pet, Rating, Messages
 
-"""
+
 class PetAdmin(admin.ModelAdmin):
-    list_display = ('owner', 'name', 'description')
+    list_display = ('name', "owner",'description')
 
 class RatingAdmin(admin.ModelAdmin):
+    list_display = ('toWho', 'madeBy', 'rating')
     #prepopulated_fields = {'slug': ('name',)}
 
 class MessagesAdmin(admin.ModelAdmin):
+    list_display = ('petId', 'seekerUsername', 'date')
     #prepopulated_fields = {'slug': ('name',)}
-"""
 
 class UserProfileAdmin(admin.ModelAdmin):
-    list_dispay = ("user", "location", "dateOfBirth", "profilePicture", "experience", "description", "showPets")
+    list_display = ("user", 'location', "dateOfBirth", 'experience', 'description')
+    #prepopulated_fields = {'slug': ('name',)}
 
 # Register your models here.
 
-admin.site.register(Pet)#, PetAdmin)
-admin.site.register(Rating)#, RatingAdmin)
-admin.site.register(Messages)#, MessagesAdmin)
+admin.site.register(Pet, PetAdmin)
+admin.site.register(Rating, RatingAdmin)
+admin.site.register(Messages, MessagesAdmin)
 admin.site.register(UserProfile, UserProfileAdmin)
