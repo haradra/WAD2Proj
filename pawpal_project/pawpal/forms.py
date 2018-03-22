ea6ea3dea2addede50981ea8cdd8f2804b3ace5d
Milo Krawiec <2162979K@student.gla.ac.uk>
2018-03-22 14:48:10 +0000

    Completed edit account, you can edit respective fields depending if you're a Pet or User

--- pawpal_project/pawpal/forms.py
+++ pawpal_project/pawpal/forms.py
@@ -48,8 +48,6 @@ class UpdatePetProfile(forms.ModelForm):
     
     username = forms.CharField(required=True)
     email = forms.EmailField(required=True)
-    first_name = forms.CharField(required=False)
-    last_name = forms.CharField(required=False)
     
 
     class Meta:
