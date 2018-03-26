de6f3c62630c8cfefb954abc952edbf097750c5f
Milo Krawiec <2162979K@student.gla.ac.uk>
2018-03-26 16:48:33 +0100

    Added basic unit test and updated gitignore

--- pawpal_project/pawpal/tests.py
+++ pawpal_project/pawpal/tests.py
@@ -4,3 +4,16 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 # Create your tests here.
+def add_user(username, email, password):
+    u = UserProfile.objects.get_or_create(user=username)[0]
+    u.email = email
+    u.password = password
+    u.save()
+    return u
+
+def add_pet(username, email, password):
+    u = Pet.objects.get_or_create(user=username)[0]
+    u.email = email
+    u.password = password
+    u.save()
+    return u
