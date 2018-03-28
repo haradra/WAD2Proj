78a00e1b4fd9c5c08ee0033e861daf4869b95793
Ivelina <ivelina.doynova@gmail.com>
2018-03-28 17:34:38 +0300

    More tests added

--- pawpal_project/pawpal/tests.py
+++ pawpal_project/pawpal/tests.py
@@ -11,7 +11,7 @@ from pawpal.urls import *
 from django.conf import settings
 import os
 
-# helper methods to create user personas
+# Helper methods to create user personas
 def add_user(username, email, password):
     u = UserProfile.objects.get_or_create(user=username)[0]
     u.email = email
@@ -19,13 +19,18 @@ def add_user(username, email, password):
     u.save()
     return u
 
-
+# Helper method to create pet
 def add_pet(username, email, password):
-    u = Pet.objects.get_or_create(user=username)[0]
+    u = PetProfile.objects.get_or_create(user=username)[0]
     u.email = email
     u.password = password
     u.save()
     return u
+
+# Helper method to create an admin
+def create_admin():
+    u = UserProfile.objects.get_or_create(username="testadmin", is_staff=True)
+    u.save()
 
 
 # The following tests check whether the urls are loaded/found successfully given the correct conditions/slugs
@@ -52,6 +57,7 @@ class PawPalUrlsTests(TestCase):
         self.assertEqual(response.status_code, 302)
 
 class TemplateTests(TestCase):
+
     # Checks whether the base template exists
     def test_base_template_exists(self):
         path_to_base = os.path.join(settings.TEMPLATE_DIR, 'base.html')
@@ -61,24 +67,50 @@ class TemplateTests(TestCase):
         response = self.client.get(reverse('home'))
         self.assertIn(reverse('home'), response.content.decode('ascii'))
 
+    # Checks if a proper message is displayed
     def test_home_displays_welcome_message(self):
-        # Checks if a proper message is displayed
         response = self.client.get(reverse('home'))
         self.assertIn("Welcome to PawPal!".lower(), response.content.decode('ascii').lower())
 
+    # Checks if base template is recognised
     def test_contact_page_using_template(self):
-        # Checks if base template is recognised
         response = self.client.get(reverse('contact'))
         self.assertTemplateUsed(response, 'base.html')
+
+class HomeViewTests(TestCase):
+    # Return a success status code if home page loads
+    def test_home_view_loads_successfully(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    # Return a success status code if about page loads
+    def test_about_view_loads_successfully(self):
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
+
+    # Return a success status code if contact us page loads
+    def test_contactus_view_loads_successfully(self):
+        response = self.client.get(reverse('contact'))
+        self.assertEqual(response.status_code, 200)
+
+    # Return a success status code if register page loads
+    def test_register_view_loads_successfully(self):
+        response = self.client.get(reverse('register'))
+        self.assertEqual(response.status_code, 200)
+
+    # Return a success status code if login page loads
+    def test_login_view_loads_successfully(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
 
 class ModelTests(TestCase):
 
     def setUp(self):
         try:
-            from populate_pawpal import populate
+            from population_script import populate
             populate()
         except ImportError:
-            print('The module populate_pawpal does not exist')
+            print('The module population_script does not exist')
         except NameError:
             print('The function populate() does not exist or is not correct')
         except:
